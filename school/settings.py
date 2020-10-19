45b81c327bc1133ad6aac21dbb48266f7767a095
mwinamijr <mwinamijr@gmail.com>
2020-10-19 23:10:41 +0300

    modified project's file to have environment variables

--- school/settings.py
+++ school/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cxz7h(wm%j269=2@#s%vljcgrk)&@((l#umniln_^oi+2fg=(v'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
