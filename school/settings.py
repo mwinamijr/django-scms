96c6090308464586cc22d11135c4957962364d2b
mwinamijr <mwinamijr@gmail.com>
2021-04-04 09:55:08 +0300

    resize pictures

--- school/settings.py
+++ school/settings.py
@@ -52,7 +52,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'build'),
+            #os.path.join(BASE_DIR, 'build'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
