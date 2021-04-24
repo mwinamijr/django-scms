d40505464952f095c5410f3217157923823574bc
mwinamijr <mwinamijr@gmail.com>
2021-04-24 13:39:54 +0300

    created new techer and accountant models

--- users/models.py
+++ users/models.py
@@ -8,6 +8,9 @@ from .managers import CustomUserManager
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=100, blank=True, null=True, verbose_name="first name")
+    middle_name = models.CharField(max_length=100, blank=True, null=True, verbose_name="middle name")
+    last_name = models.CharField(max_length=100, blank=True, null=True, verbose_name="last name")
     email = models.EmailField(_('email address'), unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
@@ -20,3 +23,19 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+class Accountant(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
+    isAccountant = models.BooleanField(default=True)
+    salary = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.email
+
+class Teacher(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
+    isTeacher = models.BooleanField(default=True)
+    salary = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.email
