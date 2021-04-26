627ca70020afb46b5bd1b2c1443d97df3d1fa9db
mwinamijr <mwinamijr@gmail.com>
2021-04-26 21:28:42 +0300

    created attendance views

--- attendance/views.py
+++ attendance/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import AttendanceStatus, TeachersAttendance
+from .serializers import (
+	AttendanceStatusSerializer, TeachersAttendanceSerializer)
+
+class AttendanceStatusViewSet(viewsets.ModelViewSet):
+	queryset = AttendanceStatus.objects.all()
+	serializer_class = AttendanceStatusSerializer
+
+class TeachersAttendanceViewSet(viewsets.ModelViewSet):
+	queryset = TeachersAttendance.objects.all()
+	serializer_class = TeachersAttendanceSerializer
+
