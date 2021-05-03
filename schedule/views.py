b8954e369910246a3190f752e1b0292622766ab3
mwinamijr <mwinamijr@gmail.com>
2021-05-03 08:40:16 +0300

    created viewSet for the schedule models

--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, views, viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
 
-# Create your views here.
+from .models import (
+    Subject, Period, DailyTimeTable, WeeklyTimeTable)
+
+from .serializers import (
+	SubjectSerializer, PeriodSerializer, DailyTimeTableSerializer, WeeklyTimeTableSerializer
+	)
+
+class SubjectViewSet(viewsets.ModelViewSet):
+	queryset = Subject.objects.all()
+	serializer_class = SubjectSerializer
+
+class PeriodViewSet(viewsets.ModelViewSet):
+	queryset = Period.objects.all()
+	serializer_class = PeriodSerializer
+
+class DailyTimeTableViewSet(viewsets.ModelViewSet):
+	queryset = DailyTimeTable.objects.all()
+	serializer_class = DailyTimeTableSerializer
+
+class WeeklyTimeTableViewSet(viewsets.ModelViewSet):
+	queryset = WeeklyTimeTable.objects.all()
+	serializer_class = WeeklyTimeTableSerializer
