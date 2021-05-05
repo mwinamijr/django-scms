6f74490f50b238594ae7f5d77c6e80d20e7876f0
mwinamijr <mwinamijr@gmail.com>
2021-05-05 21:26:19 +0300

    test git error

--- administration/models.py
+++ administration/models.py
@@ -14,6 +14,7 @@ class Article(models.Model):
 
 	def __str__(self):
 		return self.title
+        
 
 class CarouselImage(models.Model):
 	title = models.CharField(max_length=150, blank=True, null=True)
