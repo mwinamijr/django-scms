c7a55a52ade81fa462dd8bd7d08094ec20af78f2
mwinamijr <mwinamijr@gmail.com>
2021-06-17 10:49:32 +0300

    Created views for the notes app

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST
+)
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+
+class TopicViewSet(viewsets.ModelViewSet):
+    serializer_class = TopicSerializer
+    queryset = Topic.objects.all()
+
+
+class SubTopicViewSet(viewsets.ModelViewSet):
+    serializer_class = SubTopicSerializer
+    queryset = SubTopic.objects.all()
+
+
+class AssignmentViewSet(viewsets.ModelViewSet):
+    serializer_class = AssignmentSerializer
+    queryset = Assignment.objects.all()
+
+    def create(self, request):
+        serializer = AssignmentSerializer(data=request.data)
+        if serializer.is_valid():
+            assignment = serializer.create(request)
+            if assignment:
+                return Response(status=HTTP_201_CREATED)
+        return Response(status=HTTP_400_BAD_REQUEST)
+
+
+class GradedAssignmentListView(ListAPIView):
+    serializer_class = GradedAssignmentSerializer
+
+    def get_queryset(self):
+        queryset = GradedAssignment.objects.all()
+        username = self.request.query_params.get('username', None)
+        if username is not None:
+            queryset = queryset.filter(student__username=username)
+        return queryset
+
+
+class GradedAssignmentCreateView(CreateAPIView):
+    serializer_class = GradedAssignmentSerializer
+    queryset = GradedAssignment.objects.all()
+
+    def post(self, request):
+        print(request.data)
+        serializer = GradedAssignmentSerializer(data=request.data)
+        serializer.is_valid()
+        graded_assignment = serializer.create(request)
+        if graded_assignment:
+            return Response(status=HTTP_201_CREATED)
+        return Response(status=HTTP_400_BAD_REQUEST)
+
+
+class NotesListView(ListAPIView):
+    serializer_class = NoteSerializer
+    queryset = Note.objects.all()
+
