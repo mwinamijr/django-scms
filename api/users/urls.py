e791c5a3b99e5f11cfeb022f6fefa0ae2ec50588
mwinamijr <mwinamijr@gmail.com>
2021-07-11 09:49:20 +0300

    added all users urls

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from users.views import (
-	 teacherProfileView,
+	 teacherProfileView, MyTokenObtainPairView, registerUser, getUserProfile, 
+	 updateUserProfile, getUsers, getUserById, updateUser, deleteUser
 	 )
 
 urlpatterns = [
     path('teacher-profile/<int:pk>/', teacherProfileView, name="teacher-profile"),
+	path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', registerUser, name='register'),
+    path('profile/', getUserProfile, name="users-profile"),
+    path('profile/update/', updateUserProfile, name="user-profile-update"),
+    path('', getUsers, name="users"),
+    path('<str:pk>/', getUserById, name='user'),
+    path('update/<str:pk>/', updateUser, name='user-update'),
+    path('delete/<str:pk>/', deleteUser, name='user-delete'),
 ]
 
 
