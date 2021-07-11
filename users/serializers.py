117d953db93ba1ce6893dc4f7730aa2f12431405
mwinamijr <mwinamijr@gmail.com>
2021-07-11 09:48:20 +0300

    created user serializer and user serializer with token

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 from .models import Accountant, Teacher, CustomUser
 
@@ -13,6 +15,30 @@ class TeacherSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = CustomUser
-        fields = "__all__"
+        fields = ['id', 'email', 'username', 'first_name', 'middle_name', 'last_name', 'isAdmin']
+
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
+    def get_username(self, obj):
+        name = str(obj.first_name) + str(obj.last_name)
+        if name == '':
+            name = obj.email
+
+        return name
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'first_name', 'middle_name', 'last_name', 'isAdmin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
