84c2f02e205f3cf990b203a3c9a38ba90cc3dc1d
Mwinami <mwinamijr@gmail.com>
2021-11-28 19:56:45 +0300

    created finances receipt list view and receipt details view

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from rest_framework import generics, views, viewsets
+from rest_framework.decorators import api_view
+from rest_framework.parsers import FileUploadParser
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from openpyxl import load_workbook, Workbook
+from openpyxl.utils import get_column_letter
+import json
 
-# Create your views here.
+from users.models import Teacher
+from .models import Receipt
+from .serializers import (ReceiptSerializer)
+
+class ReceiptsListView(views.APIView):
+	"""
+    List all Receipts, or create a new Receipt.
+    """
+	def get(self, request, format=None):
+		receipts = Receipt.objects.all()
+		serializer = ReceiptSerializer(receipts, many=True)
+		return Response(serializer.data)
+
+	def post(self, request, format=None):
+		serializer = ReceiptSerializer(data=request.data)
+		print(request.data)
+		print(serializer.is_valid())
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ReceiptDetailView(views.APIView):
+	def get_object(self, pk):
+		try:
+			return Receipt.objects.get(pk=pk)
+		except Receipt.DoesNotExist:
+			raise Http404
+
+	def get(self, request, pk, format=None):
+		receipt = self.get_object(pk)
+		serializer = ReceiptSerializer(receipt)
+		return Response(serializer.data)
+		
+	def put(self, request, pk, format=None):
+		receipt = self.get_object(pk)
+		serializer = ReceiptSerializer(receipt, data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	
+	def delete(self, request, pk, format=None):
+		receipt = self.get_object(pk)
+		receipt.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
+
