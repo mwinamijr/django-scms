b8fc7b13240b4ec2f2b6b7859496d194dfff8e78
Mwinami <mwinamijr@gmail.com>
2021-12-16 09:48:59 +0300

    created payments serializers

--- finance/serializers.py
+++ finance/serializers.py
@@ -10,6 +10,12 @@ class AllocationSerializer(serializers.ModelSerializer):
         model = Allocation
         fields = "__all__"
 
+class PaymentAllocationSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = PaymentAllocation
+        fields = "__all__"
+
 class ReceiptSerializer(serializers.ModelSerializer):
     student = serializers.SerializerMethodField(read_only=True)
     paid_for = serializers.SerializerMethodField(read_only=True)
@@ -28,6 +34,27 @@ class ReceiptSerializer(serializers.ModelSerializer):
     def get_paid_for(self, obj):
         paid_for = obj.paid_for
         serializer = AllocationSerializer(paid_for, many=False)
+        paid_for = serializer.data['name']
+        return paid_for
+
+    def get_received_by(self, obj):
+        received_by = obj.received_by
+        serializer = AccountantSerializer(received_by, many=False)
+        received_by = serializer.data['user']
+
+        return received_by
+
+class PaymentSerializer(serializers.ModelSerializer):
+    paid_for = serializers.SerializerMethodField(read_only=True)
+    received_by = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Receipt
+        fields = "__all__"
+    
+    def get_paid_for(self, obj):
+        paid_for = obj.paid_for
+        serializer = PaymentAllocationSerializer(paid_for, many=False)
         paid_for = serializer.data['name']
         return paid_for
 
