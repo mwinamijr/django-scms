cea87a9ceb2cb3b668d2d7981b41ef2236e7190a
Mwinami <mwinamijr@gmail.com>
2021-12-16 09:49:33 +0300

    created payments urls

--- api/finance/urls.py
+++ api/finance/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from finance.views import (
-	ReceiptsListView, ReceiptDetailView,
+	ReceiptsListView, ReceiptDetailView, PaymentListView, PaymentDetailView
     )
 
 
 urlpatterns = [
 	path('receipts/', ReceiptsListView.as_view(), name="receipt-list"),
     path('receipts/<int:pk>/', ReceiptDetailView.as_view(), name="receipt-detail"),
+    path('payments/', PaymentListView.as_view(), name="payment-list"),
+    path('payments/<int:pk>/', PaymentDetailView.as_view(), name="payment-detail"),
 ]
